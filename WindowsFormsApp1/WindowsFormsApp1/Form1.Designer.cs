ec6bcb7cbe47a79b287db77bd2212d010ac30a29
prfndr <74804416+prfndr@users.noreply.github.com>
2020-11-21 08:19:31 +0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>????
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend10 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 177);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Вычислить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea9.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chart1.Legends.Add(legend9);
+            this.chart1.Location = new System.Drawing.Point(152, 43);
+            this.chart1.Name = "chart1";
+            series9.ChartArea = "ChartArea1";
+            series9.Legend = "Legend1";
+            series9.Name = "Series1";
+            this.chart1.Series.Add(series9);
+            this.chart1.Size = new System.Drawing.Size(400, 330);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea10.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea10);
+            legend10.Name = "Legend1";
+            this.chart2.Legends.Add(legend10);
+            this.chart2.Location = new System.Drawing.Point(563, 43);
+            this.chart2.Name = "chart2";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Series1";
+            this.chart2.Series.Add(series10);
+            this.chart2.Size = new System.Drawing.Size(400, 330);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "График постороенный по значениям Y, возвращаемым функцией TheFunc ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(597, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "График построенный по вычисленным коэффициентам a,b,c";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 445);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            if (!System.IO.File.Exists(fileLibrary)) //проверяем существует ли библиотека
+            {
+                MessageBox.Show("Библиотека " + fileLibrary + " не найдена!");
+                button3.Enabled = false;
+                
+
+            }
+
+
+        }
+
+
+        const string ap = "andrey parfenov";
+
+
+
+        double Opredelitel(double[,] a)
+        {
+            double otv = a[0, 0] * a[1, 1] * a[2, 2] 
+                + a[2,0] * a[0,1] * a[1,2] 
+                + a[1,0] * a[2,1] * a[0,2]
+                - a[2,0] * a[1,1] * a[0,2]
+                - a[1,0] * a[0,1] * a[2,2]
+                - a[0,0] * a[1,2] * a[2,1];
+
+            return otv;
+        }
+
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Axis ax = new Axis();
+            ax.Title = "X";
+            chart1.ChartAreas[0].AxisX = ax;
+            chart2.ChartAreas[0].AxisX = ax;
+            Axis ay = new Axis();
+            ay.Title = "Y";
+            chart1.ChartAreas[0].AxisY = ay;
+            chart2.ChartAreas[0].AxisY = ay;
+
+            chart1.Series["Series1"].LegendText = "График TheFunc";
+            chart2.Series["Series1"].LegendText = "График a,b,c";
+
+           
+
+            GetDate(); //находим коэффициенты a,b,c и строим график
+
+            FirstGr();//график строим обращаясь к функции TheFunc библиотеки
+
+        }
+
+
+        void FirstGr()//график строим обращаясь к функции TheFunc библиотеки
+        {
+
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            
+
+            double x = 0;
+            do
+            {
+
+
+                double a = TheFunc(ap, x);
+                
+                chart1.Series[0].Points.AddXY(x, a);
+                x = x + 0.5;
+
+
+            } while (x <= 10.0 + 0.00001);
+
+
+        }
+
+
+
+        const string fileLibrary = @"c:\0\Lib2-1.dll";
+        [DllImport(fileLibrary, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+
+
+        public static extern double TheFunc(String caption, double type);
+
+        void GetDate()//находим коэффициенты a,b,c и строим график
+        {
+            //берём  три произвольных значений х
+
+            double x1 = 0.0;
+            double x2 = 1.0;
+            double x3 = 2.0;
+
+            // получаем для них значения Y
+            double y1 = TheFunc(ap , x1);
+            double y2 = TheFunc(ap , x2);
+            double y3 = TheFunc(ap , x3);
+
+            // находим a,b,c, Методом Крамера
+            double[,] op =
+                {   { x1* x1,   x1, 1},
+                    { x2* x2,   x2, 1},
+                    { x3* x3,   x3, 1}
+            };
+
+            double[,] op1 =
+                {   { y1, x1, 1},
+                    { y2, x2, 1},
+                    { y3, x3, 1}
+            };
+
+            double[,] op2 =
+                {   { x1 *x1,   y1, 1},
+                    { x2 *x2,   y2, 1},
+                    { x3 *x3,   y3, 1}
+            };
+
+            double[,] op3 =
+                {   { x1* x1,   x1, y1},
+                    { x2* x2,   x2, y2},
+                    { x3* x3,   x3, y3}
+            };
+
+            textBox1.Text = "Находим коэффициенты:";
+
+            double a = Opredelitel(op1) / Opredelitel(op);
+            textBox1.Text = textBox1.Text + Environment.NewLine + "a=" + a.ToString();
+
+            double b = Opredelitel(op2) / Opredelitel(op);
+            textBox1.Text = textBox1.Text + Environment.NewLine + "b=" + b.ToString();
+
+            double c = Opredelitel(op3) / Opredelitel(op);
+            textBox1.Text = textBox1.Text + Environment.NewLine + "c=" + c.ToString();
+
+            //выводим формулу в легенду графика
+            chart2.Series["Series1"].LegendText = chart2.Series["Series1"].LegendText + Environment.NewLine
+                + a.ToString() + "x^2" + b.ToString() + "x"+c.ToString()+"=0";
+
+            //стрим график
+
+            chart2.Series[0].ChartType = SeriesChartType.Line;
+
+            double x = 0;
+            do
+            {
+
+                double y = a * x * x + b * x + c;
+
+                chart2.Series[0].Points.AddXY(x, y);
+                x = x + 0.5;
+
+
+            } while (x <= 10.0 + 0.00001);
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit(); //Выход
+        }
+    }
+}
